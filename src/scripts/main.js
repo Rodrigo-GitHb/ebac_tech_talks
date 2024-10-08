9af341dfa41f7eb31e4a520d874e7064744e45cd
Rodrigo-GitHb <rodrigo291002@hotmail.com>
2024-10-08 19:53:47 -0300

    commit data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Oct 4, 2024 19:00:00");
+const dataDoEvento = new Date("Oct 29, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
